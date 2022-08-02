48c8dadd25983bfe299b70b5391556092548a482
Guillaume Baur <guilaume.baur@gmail.com>
2022-08-02 13:59:25 +0200

    View_et_style_filtre_partenaire

---
+++ filtre_partenaire/View.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Page_des partenaires</title>
+</head>
+
+<body>
+
+<row_1>
+    <button type="button" class="btn btn-outline-success btn-lg">client_name</button>
+    <button type="button" class="btn btn-outline-success btn-lg">client_id</button>
+</row_1>
+
+<row_2>
+    <button type="button" class="btn btn-outline-success btn-lg">actif</button>
+    <button type="button" class="btn btn-outline-success btn-lg">non actif</button>
+</row_2>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Page_des partenaires</title>
+      <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <link href="style.css" rel="stylesheet" type="text/css" />
+  <link href="filtre_partenaire/style.css" rel="stylesheet" />
+
+</head>
+
+<body>
+    <?php require_once 'filtre_partenaire/View.php'  ?>
+</body>
+
+</html>
