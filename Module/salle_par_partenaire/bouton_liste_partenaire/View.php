357a9440046a0cb484f23461fa325ea2a05752e5
Guillaume Baur <guilaume.baur@gmail.com>
2022-08-02 17:31:18 +0200

    bouton_liste_partenaire_de_la_page_des_partenaires

---
+++ Module/salle_par_partenaire/bouton_liste_partenaire/View.php
@@ -0,0 +1,4 @@
+<!--View bouton_liste_partenaire-->
+<section class="bouton_liste_partenaire">
+<button type="button" class="btn btn-outline-success btn-lg"> << Liste des partenaires </button>
+</section>
