d9aa1c8da991ad320897cabb3b5dfae157b2f19d
Guillaume Baur <guilaume.baur@gmail.com>
2022-08-09 17:41:08 +0200

    front_end_filtrage_salle_par_partenaire

--- Module/salle_par_partenaire/filtre_partenaire/View.php
+++ Module/salle_par_partenaire/filtre_partenaire/View.php
@@ -1,5 +1,5 @@
   <!--Style du filtre_partenaire -->
-  <link href="../../Module/page_des_partenaires/filtre_partenaire/style.css" rel="stylesheet" />
+  <link href="../../Module/salle_par_partenaire/filtre_partenaire/style.css" rel="stylesheet" />
 
 <!--View filtre partenaire-->
 <section class="filtre_partenaire">
@@ -7,13 +7,13 @@
 <row_1>
 
 <!--Filtre client_id-->
-<button type="button" class="btn btn-outline-success btn-lg">
+<button type="button" class="aide btn btn-outline-success btn-lg">
 <form class="form" method="post" action="">
-<span> 
-  <label for="Nom">client_name:</label>
-  <input type="text" id="Nom" name="Nom">
+<span class="aide"> 
+  <label id="Nom" for="Nom">client_name:</label>
+  <input class="aide2" type="text" name="Nom">
 </span>
-  <input name="filtre" class="btn btn-outline-success btn-lg" type="submit" value="Chercher">
+  <input name="filtre" class="aide2 btn btn-outline-success btn-lg" type="submit" value="Chercher">
 </form>
     </button>
 
@@ -37,13 +37,13 @@
 
 
 <!--Filtre client_id-->
-<button type="button" class="btn btn-outline-success btn-lg">
+<button type="button" class="aide btn btn-outline-success btn-lg">
 <form class="form" method="post" action="">
-<span> 
+<span class="aide"> 
   <label for="id">client_id:</label>
-  <input type="text" id="id" name="id">
+  <input class="aide2" type="text" name="id">
 </span>
-  <input name="filtre" class="btn btn-outline-success btn-lg" type="submit" value="Chercher">
+  <input name="filtre" class="aide2 btn btn-outline-success btn-lg" type="submit" value="Chercher">
 </form>
     </button>
 
