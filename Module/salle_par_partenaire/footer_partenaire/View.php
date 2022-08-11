cf5939b4ac685375e60764aaa2d90d480dea84f9
Guillaume Baur <guilaume.baur@gmail.com>
2022-08-11 14:30:00 +0200

    view_barre_de_defilement_salle_par_partenaire

--- Module/salle_par_partenaire/footer_partenaire/View.php
+++ Module/salle_par_partenaire/footer_partenaire/View.php
@@ -1,15 +1,37 @@
    <!--Style du footer_partenaire -->
    <link href="../../Module/salle_par_partenaire/footer_partenaire/style.css" rel="stylesheet" />
 
+
 <!--View footer_partenaire-->
 <section class="footer_partenaire">
 
-<div class="btn-group">
-  <button>Previous</button>
-  <button>1</button>
-  <button>2</button>
-  <button>3</button>
-  <button>Next</button>
+<!-- Form  modifierseance-->
+<form class="barre_de_defilement" method="post" action="">
+  
+  <!--BARRE_DE_PAGE-->
+  <div class="btn-group">
+
+  <button name="plus" type="submit" class="flex_start" id="small_width" value="<?php echo $plus-'5' ?>"><<</button>
+  <button name="plus" type="submit" id="small_width" value="<?php echo $plus-'1' ?>"><</button>
+
+  <div><?php echo $plus."/".$nb_ID ?></div>
+
+  <button name="plus" type="submit" id="small_width" value="<?php echo '1'+$plus ?>">></button>
+  <button name="plus" type="submit" id="small_width" value="<?php echo '5'+$plus ?>">>></button>
+
+  </div>
+
+</form>
+
+<div class=" btn-group">
+
+<form class="btn-group barre_de_defilement" method="post" action="">
+<div class="flex_end">Aller a la page: <input name="plus2" type="number"><?php echo "/".$nb_ID ?></input></div>
+</form>
+
 </div>
 
 </section>
+
+
+
