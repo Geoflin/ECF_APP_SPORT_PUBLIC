e4c368ea3308f1c3fc65b3bcbf00b6e5cf8114ad
Guillaume Baur <guilaume.baur@gmail.com>
2022-08-11 13:54:26 +0200

    back_end_input_aller_à

--- Module/page_des_partenaires/etiquette_partenaire/View.php
+++ Module/page_des_partenaires/etiquette_partenaire/View.php
@@ -7,8 +7,15 @@ if(isset($_POST['plus'])){
   $plus= $_POST['plus'];
   if($plus<'1'){$plus='1';};
 } else {
-  $plus= '1';
+  if(isset($_POST['plus2'])){
+    $plus= $_POST['plus2'];
+    if($plus<'1'){$plus='1';};
+  }else{
+    $plus= '1';
+  };
 };
+
+
 
 //Si on appuie sur les flèches vers la gauche on va à la page n-1 de la barre de défilement des étiquettes
 $moins= ($plus-'1')*'6';
--- Module/page_des_partenaires/footer_partenaire/View.php
+++ Module/page_des_partenaires/footer_partenaire/View.php
@@ -11,26 +11,27 @@
   <!--BARRE_DE_PAGE-->
   <div class="btn-group">
 
-  <button name="plus" type="submit" id="small_width" value="<?php echo $plus-'5' ?>"><<</button>
+  <button name="plus" type="submit" class="flex_start" id="small_width" value="<?php echo $plus-'5' ?>"><<</button>
   <button name="plus" type="submit" id="small_width" value="<?php echo $plus-'1' ?>"><</button>
 
-  <button><?php echo $plus."/".$nb_ID ?></button>
+  <div><?php echo $plus."/".$nb_ID ?></div>
 
   <button name="plus" type="submit" id="small_width" value="<?php echo '1'+$plus ?>">></button>
   <button name="plus" type="submit" id="small_width" value="<?php echo '5'+$plus ?>">>></button>
 
   </div>
 
+</form>
+
 <div class=" btn-group">
 
-<button class="flex_end">Aller a la page: <input name="plus2" type="number"><?php echo "/".$nb_ID ?></input></button>
+<form class="btn-group barre_de_defilement" method="post" action="">
+<div class="flex_end">Aller a la page: <input name="plus2" type="number"><?php echo "/".$nb_ID ?></input></div>
+</form>
 
 </div>
 
 </section>
-
-</form>
-
 
 
 
