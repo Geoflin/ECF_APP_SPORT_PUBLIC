ea64c24daf02a3517718194e86581548b1b4f6d7
Guillaume Baur <guilaume.baur@gmail.com>
2022-08-12 15:58:53 +0200

    resoudre_bug_insertion_online_database

--- Pages/page_formulaire/Back_end.php
+++ Pages/page_formulaire/Back_end.php
@@ -4,7 +4,7 @@
 <?php
   $pdo = new PDO('mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_cb119347919fa64', 'b17b85233573cc', '95de3bee');
 
-/*if ($pdo->exec('INSERT INTO api_clients (actif, client_name, password, active, short_description, full_description, urll, mail) VALUES ("'. $_POST['actif'] . '", "'. $_POST['client_name'] . '", "'. $_POST['password'] . '", "On", "'. $_POST['short_description'] . '", "'. $_POST['full_description'] . '", "'. $_POST['urll'] . '", "'. $_POST['mail'] . '");') !== false){};*/
+if ($pdo->exec('INSERT INTO api_clients (actif, client_name, password, active, short_description, full_description, urll, mail) VALUES ("'. $_POST['actif'] . '", "'. $_POST['client_name'] . '", "'. $_POST['password'] . '", "On", "'. $_POST['short_description'] . '", "'. $_POST['full_description'] . '", "'. $_POST['urll'] . '", "'. $_POST['mail'] . '");') !== false){};
 
 //Envoie du mail
 define('MAIL_DESTINATAIRE','geoffrey.marhoffer@gmail.com'); // remplacer par votre email
