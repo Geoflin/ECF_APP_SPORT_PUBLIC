35b89975ab85cd63693f1d0d55a5bc7b5882bce3
Guillaume Baur <guilaume.baur@gmail.com>
2022-08-12 11:04:09 +0200

    test_heroku_envoie_mail

--- Pages/page_formulaire/Back_end.php
+++ Pages/page_formulaire/Back_end.php
@@ -4,7 +4,7 @@
 <?php
   $pdo = new PDO('mysql:host=localhost;dbname=sport', 'root', '');
 
-if ($pdo->exec('INSERT INTO api_clients (actif, client_name, password, active, short_description, full_description, urll, mail) VALUES ("'. $_POST['actif'] . '", "'. $_POST['client_name'] . '", "'. $_POST['password'] . '", "On", "'. $_POST['short_description'] . '", "'. $_POST['full_description'] . '", "'. $_POST['urll'] . '", "'. $_POST['mail'] . '");') !== false){};
+/*if ($pdo->exec('INSERT INTO api_clients (actif, client_name, password, active, short_description, full_description, urll, mail) VALUES ("'. $_POST['actif'] . '", "'. $_POST['client_name'] . '", "'. $_POST['password'] . '", "On", "'. $_POST['short_description'] . '", "'. $_POST['full_description'] . '", "'. $_POST['urll'] . '", "'. $_POST['mail'] . '");') !== false){};*/
 
 //Envoie du mail
 define('MAIL_DESTINATAIRE','geoffrey.marhoffer@gmail.com'); // remplacer par votre email
