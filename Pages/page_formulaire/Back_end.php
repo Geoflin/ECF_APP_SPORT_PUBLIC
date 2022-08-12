429df2bcab31ff955c722611d618298c6f37c210
Guillaume Baur <guilaume.baur@gmail.com>
2022-08-12 16:24:35 +0200

    pre_sendinblue

--- Pages/page_formulaire/Back_end.php
+++ Pages/page_formulaire/Back_end.php
@@ -67,3 +67,33 @@ if (mail(MAIL_DESTINATAIRE,MAIL_SUJET,$mail_corps,$mail_entete)) {
     margin: 10% 10% 0% 10%;
   }
 </style>
+
+<?php 
+//test sendinBlue
+require_once(__DIR__ . '/vendor/autoload.php');
+
+// Configure API key authorization: api-key
+$config = SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey('api-key', 'YOUR_API_KEY');
+
+// Uncomment below line to configure authorization using: partner-key
+// $config = SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey('partner-key', 'YOUR_API_KEY');
+
+$apiInstance = new SendinBlue\Client\Api\TransactionalEmailsApi(
+    // If you want use custom http client, pass your client which implements `GuzzleHttp\ClientInterface`.
+    // This is optional, `GuzzleHttp\Client` will be used as default.
+    new GuzzleHttp\Client(),
+    $config
+);
+$sendSmtpEmail = new \SendinBlue\Client\Model\SendSmtpEmail(); // \SendinBlue\Client\Model\SendSmtpEmail | Values to send a transactional email
+$sendSmtpEmail['to'] = array(array('email'=>'testmail@example.com', 'name'=>'John Doe'));
+$sendSmtpEmail['templateId'] = 59;
+$sendSmtpEmail['params'] = array('name'=>'John', 'surname'=>'Doe');
+$sendSmtpEmail['headers'] = array('X-Mailin-custom'=>'custom_header_1:custom_value_1|custom_header_2:custom_value_2');
+
+try {
+    $result = $apiInstance->sendTransacEmail($sendSmtpEmail);
+    print_r($result);
+} catch (Exception $e) {
+    echo 'Exception when calling TransactionalEmailsApi->sendTransacEmail: ', $e->getMessage(), PHP_EOL;
+}
+?>
