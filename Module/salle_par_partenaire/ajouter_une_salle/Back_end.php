91a24a510e1f6fc50584e988f6502fd82c1c73fe
Guillaume Baur <guilaume.baur@gmail.com>
2022-08-22 17:59:52 +0200

    checkpoint

--- Module/salle_par_partenaire/ajouter_une_salle/Back_end.php
+++ Module/salle_par_partenaire/ajouter_une_salle/Back_end.php
@@ -8,10 +8,8 @@ require_once "../../../env/secret2.php";
     integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
 <?php
-/*
 if ($pdo->exec('INSERT INTO salle_de_sport3 (client_id, Nom, branche, zones) VALUES ("'. $_POST['client_id'] . '", "'. $_POST['Nom'] . '", "'. $_POST['branche'] . '", "'. $_POST['zones'] . '");') !== false){};
 if ($pdo->exec('INSERT INTO api_install_perm (client_id, members_read, members_write, members_add, members_products_add, members_payment_schedules_read, members_statistiques_read, members_subscription_read, payment_schedules_read, payment_schedules_write, payment_day_read) VALUES ("'. $_POST['client_id'] . '", "'. $_POST['members_read'] . '", "'. $_POST['members_write'] . '", "'. $_POST['members_add'] . '", "'. $_POST['members_products_add'] . '" , "'. $_POST['members_payment_schedules_read'] . '" , "'. $_POST['members_statistiques_read'] . '" , "'. $_POST['members_subscription_read'] . '", "'. $_POST['payment_schedules_read'] . '", "'. $_POST['payment_schedules_write'] . '", "'. $_POST['payment_day_read'] . '");') !== false){};
-*/
 ?>
 
 <form method="POST" action="../../../Pages/salle_par_partenaire/View.php">
@@ -42,5 +40,5 @@ h3 {
 }
 </style>
 
-<?php //require_once '../../mail/Ajout_salle_de_sport.php' ?>
+<?php require_once '../../mail/Ajout_salle_de_sport.php' ?>
 <?php require_once '../../twig/index.php' ?>
